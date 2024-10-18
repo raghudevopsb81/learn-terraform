89ae57d8bf81c5344314717e23a5f520269499bb
Raghu K <raghuk.vit@gmail.com>
2024-10-18 08:49:49 +0530

    Ansible project

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,4 +1,10 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "frontend"
+  }
 }
+
+
