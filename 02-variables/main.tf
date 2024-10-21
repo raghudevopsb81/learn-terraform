20ce8aeadf26e639b6a2a9f9a195f19c5537e1dc
Raghu K <raghuk.vit@gmail.com>
2024-10-21 08:26:49 +0530

    Ansible project

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -62,6 +62,7 @@ output "list1_1" {
 
 output "map1_aws_trainer" {
   value = var.map1["aws"]["trainer"]
+  #value = var.map1["aws"].trainer
 }
 
 # Declare a empty variable and we can get the value from CLI, Unlike ansible or shell we must and should declare a variable in order to use it.
