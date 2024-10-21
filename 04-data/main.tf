fa4084f495ca12a9d9506bf22a6d7a3cbb67b142
Raghu K <raghuk.vit@gmail.com>
2024-10-21 08:48:01 +0530

    Ansible project

--- 04-data/main.tf
+++ 04-data/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
   ami           = data.aws_ami.ami.id
   instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags = {
     Name = "test"
   }
