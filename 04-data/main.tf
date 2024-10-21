b6fe852e47367509fc6683b13e8b072feeee2df0
Raghu K <raghuk.vit@gmail.com>
2024-10-21 08:45:13 +0530

    Ansible project

---
+++ 04-data/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "test"
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "RHEL-9-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
