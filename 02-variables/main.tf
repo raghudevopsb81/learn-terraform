11dc161dc9a50db67fa499b6ffa0540c452e14a0
Raghu K <raghuk.vit@gmail.com>
2024-10-21 07:51:08 +0530

    Ansible project

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -12,4 +12,57 @@ output "URL1" {
   value = "URL - ${var.URL}"
 }
 
+# Data types
+variable "num1" {
+  default = 10
+}
+
+variable "bool1" {
+  default = true
+}
+
+variable "str1" {
+  default = "Hello"
+}
+
+# Numbers, Booleans & Strings
+
+# Variable types
+variable "str2" {
+  default = "World"
+}
+
+variable "list1" {
+  default = [
+    10,
+    20,
+    "xyz",
+    false
+  ]
+}
+
+# List can have combination of multiple data types also.
+
+variable "map1" {
+  default = {
+    aws = {
+      trainer = "John"
+      duration = 30
+    }
+    devops = {
+      trainer = "Steve"
+      duration = 20
+    }
+  }
+}
+
+output "list1_1" {
+  value = var.list1[2]
+}
+
+output "map1_aws_trainer" {
+  value = var.map1["aws"]["trainer"]
+}
+
+
 
