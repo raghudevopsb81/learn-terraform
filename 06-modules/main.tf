e285025d522defebf26c2165de4dc70a29c9848a
Raghu K <raghuk.vit@gmail.com>
2024-10-22 07:48:09 +0530

    Ansible project

--- 06-modules/main.tf
+++ 06-modules/main.tf
@@ -16,12 +16,12 @@ output "ip" {
   value = module.ec2
 }
 
-# module "route53" {
-#   count = length(var.instances)
-#   source = "./route53"
-#   instance_name = var.instances[count.index]
-#   ip_address = module.ec2.ip_address[count.index]
-# }
+module "route53" {
+  count = length(var.instances)
+  source = "./route53"
+  instance_name = var.instances[count.index]
+  ip_address = module.ec2[count.index].ip_address
+}
 
 
 
