a0d916c75b0adda7f269bcd09181a5c3a4e77fbc
Raghu K <raghuk.vit@gmail.com>
2024-10-25 08:07:37 +0530

    Terraform

---
+++ 09-vault/main.tf
@@ -0,0 +1,14 @@
+provider "vault" {
+  address = "https://172.31.36.180:8200"
+  token   = var.vault_token
+  skip_tls_verify = true
+}
+
+variable "vault_token" {}
+
+
+data "vault_kv_secret_v2" "example" {
+  name  = "my_credentials"
+  mount = "test"
+}
+
