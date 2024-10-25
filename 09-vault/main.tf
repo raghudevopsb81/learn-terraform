52e4c7a618cb893abac1cbd63446ccce3ba29753
Raghu K <raghuk.vit@gmail.com>
2024-10-25 08:17:40 +0530

    Terraform

--- 09-vault/main.tf
+++ 09-vault/main.tf
@@ -6,14 +6,17 @@ provider "vault" {
 
 variable "vault_token" {}
 
-
-data "vault_kv_secret_v2" "example" {
-  name  = "my_credentials"
-  mount = "test"
+data "vault_generic_secret" "example" {
+  path = "test/my_credentials"
 }
 
+# data "vault_kv_secret_v2" "example" {
+#   name  = "my_credentials"
+#   mount = "test"
+# }
+
 resource "local_file" "foo" {
-  content  = data.vault_kv_secret_v2.example.data_json["password"]
+  content  = data.vault_generic_secret.example.data["password"]
   filename = "/tmp/secret"
 }
 
