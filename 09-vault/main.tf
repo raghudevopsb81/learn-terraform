89db76a1722b19a999ba8c6a1852735719947e79
Raghu K <raghuk.vit@gmail.com>
2024-10-25 08:10:43 +0530

    Terraform

--- 09-vault/main.tf
+++ 09-vault/main.tf
@@ -12,8 +12,11 @@ data "vault_kv_secret_v2" "example" {
   mount = "test"
 }
 
-resource "local_file" "foo" {
-  content  = data.vault_kv_secret_v2.example.data_json
-  filename = "/tmp/secret"
-}
+# resource "local_file" "foo" {
+#   content  = data.vault_kv_secret_v2.example.data_json
+#   filename = "/tmp/secret"
+# }
 
+output "json" {
+  value = data.vault_kv_secret_v2.example.data_json
+}
